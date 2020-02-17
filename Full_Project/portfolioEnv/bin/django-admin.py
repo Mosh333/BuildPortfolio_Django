324297c1b9ed8fd2f64a0327def7fd7ff47ceba1
Moshiur <howladermoshiur@gmail.com>
2020-02-17 03:10:15 -0500

    Full Project Commit
    Full Complete Project

---
+++ Full_Project/portfolioEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/namehere/Documents/Github/BuildPortfolio_Django/Full_Project/portfolioEnv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
